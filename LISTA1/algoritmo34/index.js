a8cc0e4a86a4b6d80ad4e2c6c2f29c5145531ec9
Leandro Barbosa <lehbarbosa87@gmail.com>
2023-12-21 20:10:56 -0300

    Up ex

---
+++ LISTA1/algoritmo34/index.js
@@ -0,0 +1,7 @@
+// Ler um número inteiro e imprimir seu sucessor e seu antecessor.
+const numero = parseInt(prompt('Digite um númeiro: '))
+
+const antecessor = numero - 1
+const sucessor = numero + 1
+
+alert(`O sucessor de ${numero} é =>  ${sucessor}\nO antecessor de ${numero} é =>  ${antecessor}`)
