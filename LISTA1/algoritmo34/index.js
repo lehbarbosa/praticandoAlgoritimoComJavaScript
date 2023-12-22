5f126d7281ace7e82d5d5d738ba71d859d276ed8
Leandro Barbosa <lehbarbosa87@gmail.com>
2023-12-22 13:16:39 -0300

    corrigido algoritmo34

--- LISTA1/algoritmo34/index.js
+++ LISTA1/algoritmo34/index.js
@@ -1,5 +1,5 @@
 // Ler um número inteiro e imprimir seu sucessor e seu antecessor.
-const numero = parseInt(prompt('Digite um númeiro: '))
+const numero = parseInt(prompt('Digite um número: '))
 
 const antecessor = numero - 1
 const sucessor = numero + 1
